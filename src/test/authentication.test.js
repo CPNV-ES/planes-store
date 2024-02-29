310f75d89cfb574d1a19eeabf339bfcae450ff0a
Benjamin Fontana <ben192003@gmail.com>
2024-02-29 12:55:19 +0100

    refactor: remove a test and change the mock logic

--- src/test/authentication.test.js
+++ src/test/authentication.test.js
@@ -40,73 +40,59 @@ FBConnector.mockImplementation(() => {
 });
 
 describe("When Authentication is used with facebook connector", () => {
-	describe("When all requests are valid", () => {
-		Authentication.mockImplementation(() => {
-			const FB = new FBConnector();
-			res = undefined;
-			return {
-				login: () => {
-                    this.res = FB.login();
-                    if (this.res.status !== "connected") {
-                        this.res = undefined;
-                        throw new Error("UnauthorizedError");
-					}
-					return this.res.status;
-				},
-				logout: () => {
-					this.res = FB.logout();
-				},
-				isUserLoggedIn: () => {
-					if (this.res?.status == "connected") {
-						return true;
-					} else {
-						return false;
-					}
-				},
-			};
-		});
-		auth = new Authentication();
-		describe("When authentication authorize connection", () => {
-			it("should log in the user", () => {
-				// Given
-				// User is not logged in
-				const auth = new Authentication();
-
-				// When
-				// User clicks has clicked on the login button and has entered the correct credentials
-				auth.login();
-
-				// Then
-				// User is logged in
-				expect(auth.isUserLoggedIn()).toBe(true);
-			});
-			it("should log out the user", () => {
-				// Given
-				// User is logged in
-				const auth = new Authentication();
-				auth.login();
-
-				// When
-				// User clicks on the logout button
-				auth.logout();
-
-				// Then
-				// User is logged out
-				expect(auth.isUserLoggedIn()).toBe(false);
-			});
-		});
+	Authentication.mockImplementation(() => {
+		const FB = new FBConnector();
+		res = undefined;
+		return {
+			login: () => {
+				this.res = FB.login();
+				if (this.res.status !== "connected") {
+					this.res = undefined;
+					throw new Error("UnauthorizedError");
+				}
+				return this.res.status;
+			},
+			logout: () => {
+				this.res = FB.logout();
+			},
+			isUserLoggedIn: () => {
+				if (this.res?.status == "unknown") {
+					return false;
+				}
+				this.res = FB.checkUserLoginStatus();
+				if (this.res?.status == "connected") {
+					return true;
+				}
+			},
+		};
 	});
-	describe("When Authentication failed", () => {
-		it("should be false if user is not logged in when accessing a page that requires authentication", () => {
+	auth = new Authentication();
+	describe("When authentication authorize connection", () => {
+		it("should log in the user", () => {
 			// Given
 			// User is not logged in
 			const auth = new Authentication();
 
 			// When
-			// User tries to access a page that requires authentication
+			// User clicks has clicked on the login button and has entered the correct credentials
+			auth.login();
 
 			// Then
-			// An error is thrown
+			// User is logged in
+			expect(auth.isUserLoggedIn()).toBe(true);
+		});
+		it("should log out the user which is logged in", () => {
+			// Given
+			// User is logged in
+			const auth = new Authentication();
+			auth.login();
+
+			// When
+			// User clicks on the logout button
+			auth.logout();
+
+			// Then
+			// User is logged out
 			expect(auth.isUserLoggedIn()).toBe(false);
 		});
 	});
