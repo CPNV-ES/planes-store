bca2788fe80e1efba4794b4b7051115f40fad8f0
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-02-29 11:33:25 +0100

    feat: import js from template

--- login/login.js
+++ login/login.js
@@ -1,1 +1,16 @@
 import './login.scss'
+import $ from 'jquery';
+
+$(function() {
+    'use strict';
+
+    $('.form-control').on('input', function() {
+        var $field = $(this).closest('.form-group');
+        if (this.value) {
+            $field.addClass('field--not-empty');
+        } else {
+            $field.removeClass('field--not-empty');
+        }
+    });
+
+});
