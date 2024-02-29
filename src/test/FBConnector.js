328fb96892e2ff5b8a83665f2ef9e8d965dd0aac
Benjamin Fontana <ben192003@gmail.com>
2024-02-29 12:46:38 +0100

    test: refactor and rewrite tests

--- src/test/FBConnector.js
+++ src/test/FBConnector.js
@@ -1,3 +1,5 @@
+const Connector = require('./Connector');
+
 module.exports = class FBConnector extends Connector {
 	login() {}
 	logout() {}
--- src/test/authentication.test.js
+++ src/test/authentication.test.js
@@ -1,105 +1,127 @@
 const Authentication = require("./Authentication");
+const FBConnector = require("./FBConnector");
 
 jest.mock("./Authentication");
+jest.mock("./FBConnector");
 
-describe("When Authentication throws an error", () => {
-	beforeEach(() => {
-		Authentication.mockImplementation(() => {
-			return {
-				login: jest.fn(),
-				logout: jest.fn(),
-				checkUserLoginStatus: jest.fn(() => {
-					throw new Error("UserNotConnectedError");
-				}),
-			};
-		});
-	});
-	it("should throw an error if the user is not connected when accessing a page that requires authentication", () => {
-		// Given
-		// User is not logged in
-		const auth = new Authentication();
-
-		// When
-		// User tries to access a page that requires authentication
-
-		// Then
-		// An error is thrown
-		expect(auth.checkUserLoginStatus).toThrowError("UserNotConnectedError");
-	});
+FBConnector.mockImplementation(() => {
+	return {
+		login: () => ({
+			status: "connected",
+			authResponse: {
+				accessToken: "{access-token}",
+				expiresIn: "{unix-timestamp}",
+				reauthorize_required_in: "{seconds-until-token-expires}",
+				signedRequest: "{signed-parameter}",
+				userID: "{user-id}",
+			},
+		}),
+		logout: () => ({
+			status: "unknown",
+			authResponse: {
+				accessToken: "{access-token}",
+				expiresIn: "{unix-timestamp}",
+				reauthorize_required_in: "{seconds-until-token-expires}",
+				signedRequest: "{signed-parameter}",
+				userID: "{user-id}",
+			},
+		}),
+		checkUserLoginStatus: () => ({
+			status: "connected",
+			authResponse: {
+				accessToken: "{access-token}",
+				expiresIn: "{unix-timestamp}",
+				reauthorize_required_in: "{seconds-until-token-expires}",
+				signedRequest: "{signed-parameter}",
+				userID: "{user-id}",
+			},
+		}),
+	};
 });
 
-describe("When authentication authorize connection", () => {
-	beforeEach(() => {
+describe("When Authentication is used with facebook connector", () => {
+	describe("When all requests are valid", () => {
 		Authentication.mockImplementation(() => {
-			let status = "";
+			const FB = new FBConnector();
+			res = undefined;
 			return {
-				login: jest.fn(() => {
-					status = "connected";
-				}),
-				logout: jest.fn(() => {
-					status = "not connected";
-				}),
-				checkUserLoginStatus: jest.fn(() => {
-					return status;
-				}),
+				login: () => {
+                    this.res = FB.login();
+                    if (this.res.status !== "connected") {
+                        this.res = undefined;
+                        throw new Error("UnauthorizedError");
+					}
+					return this.res.status;
+				},
+				logout: () => {
+					this.res = FB.logout();
+				},
+				isUserLoggedIn: () => {
+					if (this.res?.status == "connected") {
+						return true;
+					} else {
+						return false;
+					}
+				},
 			};
 		});
-	});
-	it("should log in the user", () => {
-		// Given
-		// User is not logged in
-		const auth = new Authentication();
+		auth = new Authentication();
+		describe("When authentication authorize connection", () => {
+			it("should log in the user", () => {
+				// Given
+				// User is not logged in
+				const auth = new Authentication();
 
-		// When
-		// User clicks has clicked on the login button and has entered the correct credentials
-		auth.login();
+				// When
+				// User clicks has clicked on the login button and has entered the correct credentials
+				auth.login();
 
-		// Then
-		// User is logged in
-		expect(auth.checkUserLoginStatus()).toBe("connected");
-	});
-	it("should log out the user", () => {
-		// Given
-		// User is logged in
-		const auth = new Authentication();
-		auth.login();
+				// Then
+				// User is logged in
+				expect(auth.isUserLoggedIn()).toBe(true);
+			});
+			it("should log out the user", () => {
+				// Given
+				// User is logged in
+				const auth = new Authentication();
+				auth.login();
 
-		// When
-		// User clicks on the logout button
-		auth.logout();
+				// When
+				// User clicks on the logout button
+				auth.logout();
 
-		// Then
-		// User is logged out
-		expect(auth.checkUserLoginStatus()).toBe("not connected");
-	});
-});
-
-describe("When authentication do not authorize connection", () => {
-	beforeEach(() => {
-		Authentication.mockImplementation(() => {
-			let status = "";
-			return {
-				login: jest.fn(() => {
-					status = "not connected";
-					throw new Error("UnauthorizedError");
-				}),
-				checkUserLoginStatus: jest.fn(() => {
-					return status;
-				}),
-			};
+				// Then
+				// User is logged out
+				expect(auth.isUserLoggedIn()).toBe(false);
+			});
 		});
 	});
-	it("should not log in the user", () => {
-		// Given
-		// User is not logged in
-		const auth = new Authentication();
+	describe("When Authentication failed", () => {
+		it("should be false if user is not logged in when accessing a page that requires authentication", () => {
+			// Given
+			// User is not logged in
+			const auth = new Authentication();
 
-		// When
-		// User has clicked on the login button and has entered the wrong credentials
+			// When
+			// User tries to access a page that requires authentication
 
-		// Then
-		// User is not logged in
-		expect(auth.login).toThrowError("UnauthorizedError");
-		expect(auth.checkUserLoginStatus()).toBe("not connected");
+			// Then
+			// An error is thrown
+			expect(auth.isUserLoggedIn()).toBe(false);
+		});
+	});
+	describe("When authentication do not authorize connection", () => {
+		it("should not log in the user", () => {
+			// Given
+			// User is not logged in
+			const auth = new Authentication();
+
+			// When
+			// User has clicked on the login button and has entered the wrong credentials
+
+			// Then
+			// User is not logged in
+			expect(auth.isUserLoggedIn()).toBe(false);
+		});
 	});
 });
