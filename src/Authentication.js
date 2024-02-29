c982f544844f8d9a9203357c40e5fae2dcd3582b
Benjamin Fontana <ben192003@gmail.com>
2024-02-29 12:59:51 +0100

    chore: remove wrong placeholders

--- src/Authentication.js
+++ src/Authentication.js
@@ -1,14 +1,6 @@
 module.exports = class Authentication {
-    Authentication(connector) {
-        this.connector = new connector;
-    }
-    login() {
-        this.connector.login();
-    }
-    logout() { 
-        this.connector.logout();
-    }
-    isUserLoggedIn() { 
-        return this.connector.checkUserLoginStatus();
-    }
-}
+	Authentication(connector) {}
+	login() {}
+	logout() {}
+	isUserLoggedIn() {}
+};
