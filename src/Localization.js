834e49e401c99f4e6de58c9a4f1598f13ac8ae90
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-03-14 08:27:33 +0100

    feat: remove unnecessary context property and remove logic from main class

--- src/Localization.js
+++ src/Localization.js
@@ -1,22 +1,14 @@
+const i18next = require('i18next')
 module.exports = class Localization {
     language
-    context
 
-    constructor(context = document) {
+    /**
+     * @property {i18next}
+     */
+    i18n
+
+    constructor(i18n) {
+        this.language = 'fr'
+        this.i18n = i18n
     }
-
-    update() {
-        this.context.querySelector('html').setAttribute('lang', language)
-        this.context.querySelectorAll('[data-lang-text]').forEach(element => {
-            element.textContent = this.i18n.t(element.getAttribute('data-lang-text'))
-        })
-        this.context.querySelectorAll('[data-lang-attr]').forEach(element => {
-            const parts = element.getAttribute('data-lang-attr').split(':')
-            element.setAttribute(parts[0], this.i18n.t(parts[1]))
-        })
-    }
-
-    setLanguage(language) {
-    }
-
 }
