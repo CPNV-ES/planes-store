c5aa245ee925cdc9f5362a22931e921583475d4a
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-03-14 08:28:51 +0100

    test: update to implement logic of main class and fix translation value, and make use of virtual dom

--- src/test/localization/localization.test.js
+++ src/test/localization/localization.test.js
@@ -1,10 +1,7 @@
-const Localization = require('../../Localization')
-const VirtualDom = require('./virtual-test-dom')
-const i18next = require('i18next')
-
 jest.mock('../../Localization')
-
-const vDOM = new VirtualDom()
+const Localization = require('../../Localization')
+const i18next = require('i18next')
+const VirtualDom = require('./virtual-test-dom')
 
 i18next.init({
     lng: 'fr',
@@ -23,11 +20,11 @@ i18next.init({
                 'head.title': 'My awesome title',
                 'title': 'My awesome title',
                 'input.label': 'Lastname',
-                'input.placeholder': 'name'
+                'input.placeholder': 'Name'
             }
         },
         de: {
-            translation: {
+            translations: {
                 'head.title': 'Mein toller Titel',
                 'title': 'Mein toller Titel',
                 'input.label': 'Nachname',
@@ -38,23 +35,35 @@ i18next.init({
 })
 
 Localization.mockImplementation(() => {
-    return {
-        language: 'fr',
-        context: vDOM.vDOM,
-        setLanguage: (language) => {
-            this.language = language
-            i18next.changeLanguage(language)
-            console.log(this)
-        },
+    this.i18n = i18next
+
+    this.update = () => {
+        document.querySelector('html').setAttribute('lang', this.language)
+        document.querySelectorAll('[data-lang-text]').forEach(element => {
+            element.textContent = this.i18n.t(element.getAttribute('data-lang-text'))
+        })
+        document.querySelectorAll('[data-lang-attr]').forEach(element => {
+            const parts = element.getAttribute('data-lang-attr').split(':')
+            element.setAttribute(parts[0], this.i18n.t(parts[1]))
+        })
     }
-})
+
+    return {
+        setLanguage: async (language) => {
+            this.language = language
+            await this.i18n.changeLanguage(language)
+            this.update()
+        },
+    }})
+
+const vDOM = new VirtualDom()
 
 describe("When change language from FR to EN", () => {
-    it('should translate the page to english', () => {
-        const loc = new Localization()
-        loc.setLanguage('en')
-        expect(loc.context.getTitleContent().textContent).toBe('My awesome title')
-        expect(loc.context.getLabelContent().textContent).toBe('Lastname')
-        expect(loc.context.getInputPlaceholder()).toBe('name')
+    it('should translate the page to english', async () => {
+        const loc = new Localization(document, i18next)
+        await loc.setLanguage('en')
+        expect(vDOM.getTitleContent().textContent).toBe('My awesome title')
+        expect(vDOM.getLabelContent().textContent).toBe('Lastname')
+        expect(vDOM.getInputPlaceholder()).toBe('Name')
     });
 })
