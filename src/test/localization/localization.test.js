600b3f3f278349e66675815d5aa8a7811f57c1aa
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-03-20 19:05:14 +0100

    feat: add tests of the implementation of i18n

--- src/test/localization/localization.test.js
+++ src/test/localization/localization.test.js
@@ -18,7 +18,57 @@ describe("When user change the language of the page", () => {
         let languageSelector = new Select(await driver.findElement(By.id('language-selector')));
         await languageSelector.selectByValue('en')
 
-        expect(driver.findElement(By.css('[data-lang-text="hero.tagline"]')).getText()).toBe('The future of private aviation is already here')
+        let recievedText = await driver.findElement(By.css('h1.subtitle')).getText();
+
+        expect(recievedText).toBe('The future of private aviation is already here')
+    });
+
+    it('should translate the page to french', async () => {
+        await driver.get(APP_URL);
+
+        let languageSelector = new Select(await driver.findElement(By.id('language-selector')));
+        await languageSelector.selectByValue('fr')
+
+        let recievedText = await driver.findElement(By.css('h1.subtitle')).getText();
+
+        expect(recievedText).toBe('Le futur de l\'aviation privée est déjà là')
+    });
+
+    it('should translate the page to german', async () => {
+        await driver.get(APP_URL);
+
+        let languageSelector = new Select(await driver.findElement(By.id('language-selector')));
+        await languageSelector.selectByValue('de')
+
+        let recievedText = await driver.findElement(By.css('h1.subtitle')).getText();
+
+        expect(recievedText).toBe('Die Zukunft der Privatfliegerei ist bereits da')
+    });
+
+    it('should translate the page to italian', async () => {
+        await driver.get(APP_URL);
+
+        let languageSelector = new Select(await driver.findElement(By.id('language-selector')));
+        await languageSelector.selectByValue('it')
+
+        let recievedText = await driver.findElement(By.css('h1.subtitle')).getText();
+
+        expect(recievedText).toBe('Il futuro dell\'aviazione privata è già qui')
+    });
+
+    it("should'nt whipe input data while translating page", async () => {
+        await driver.get(APP_URL);
+
+        const inputContent = "Test input content"
+
+        let input = await driver.findElement(By.id('exampleInputEmail1'));
+
+        await input.sendKeys(inputContent)
+
+        let languageSelector = new Select(await driver.findElement(By.id('language-selector')));
+        await languageSelector.selectByValue('it')
+
+        expect(await input.getAttribute("value")).toBe(inputContent)
     });
 
     afterAll(async () => {
