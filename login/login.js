a3c08bfa4f3168ddbd51a02dd54dbb6940c08166
Benjamin Fontana <ben192003@gmail.com>
2024-03-27 19:47:53 +0100

    feat: add event on click on login/logout link

--- login/login.js
+++ login/login.js
@@ -1,18 +1,25 @@
-import './login.scss'
-import FBConnector from '../src/models/FBConnector'
+import "./login.scss";
+import $ from "jquery";
+import FBConnector from "../src/models/FBConnector";
 
-const connector = new FBConnector
+const connector = new FBConnector();
 
-$(function() {
-    'use strict';
+$(function () {
+	"use strict";
 
-    $('.form-control').on('input', function() {
-        var $field = $(this).closest('.form-group');
-        if (this.value) {
-            $field.addClass('field--not-empty');
-        } else {
-            $field.removeClass('field--not-empty');
-        }
-    });
+	$(".form-control").on("input", function () {
+		var $field = $(this).closest(".form-group");
+		if (this.value) {
+			$field.addClass("field--not-empty");
+		} else {
+			$field.removeClass("field--not-empty");
+		}
+	});
+});
 
+$("#facebook-login-btn").on("click", function () {
+	connector.login();
+});
+$("#facebook-logout-btn").on("click", function () {
+	connector.logout();
 });
