b5eaa68db2d93a87298b31688c63e4fdd67cf4d9
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-03-28 11:29:41 +0100

    feat: simplify TranslationStore due to dictionary removal

--- src/models/TranslationStore.js
+++ src/models/TranslationStore.js
@@ -1,34 +1,31 @@
 import LocalStorageUnsupportedException from "../exceptions/LocalStorageUnsupportedException";
-import Dictionary from "./Dictionary";
 
-export default class TranslationStore {
+export default class DictionaryStore {
 
-    constructor() {
+    constructor(namespace) {
         if (!window.localStorage) throw new LocalStorageUnsupportedException();
-        this.dictionary = this.getDictionary();
-        if (this.dictionary.isEmpty()) {
-            this.dictionary = new Dictionary();
-            this.storeDictionary();
+        if (this.dictionary.length === 0) {
+            this.dictionary = dictionary[namespace] ?? {}
         }
     }
 
-    storeDictionary = (dictionary) => {
+    #storeDictionary = (dictionary) => {
         window.localStorage.setItem('dictionary', JSON.stringify(dictionary ?? this.dictionary));
     }
 
     get dictionary() {
-        return this.getDictionary;
+        return this.#getDictionary;
     }
 
     set dictionary(dictionary) {
-        this.storeDictionary(dictionary);
+        this.#storeDictionary(dictionary);
     }
 
     /**
      * @return {Dictionary}
      */
-    getDictionary = () => {
-        return Dictionary.createFromObject(JSON.parse(window.localStorage.getItem('dictionary') ?? '{}'));
+    #getDictionary = () => {
+        return JSON.parse(window.localStorage.getItem('dictionary') ?? '{}');
     }
 
 }
