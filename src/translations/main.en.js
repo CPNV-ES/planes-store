565ef4f0102172b20637ada299242385c23893de
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-03-28 11:27:48 +0100

    feat: create translation main domain files

---
+++ src/translations/main.en.js
@@ -0,0 +1,44 @@
+export default {
+    nav: {
+        contact: "Contact us",
+        vat: "All prices are HT",
+        menu: {
+            product_index: "Our planes",
+            ask: "Ask information"
+        }
+    },
+    hero: {
+        tagline: "The future of private aviation is already here",
+        cta: "Access to the future"
+    },
+    product_index: {
+        title: "Our planes",
+        tagline: "Featured",
+    },
+    product: {
+        price: "From {value} CHF",
+    },
+    ask: {
+        name: {
+            placeholder: "Your name",
+            label: "Full name"
+        },
+        phone: {
+            placeholder: "Your phone number",
+            label: "Phone number"
+        },
+        content: {
+            placeholder: "Your request",
+            label: "Message"
+        },
+        button: "Send",
+        policy: {
+            label: "We don't send spam to customers. Check our",
+            link: "fr",
+            link_label: "Privacy policy"
+        }
+    },
+    footer: {
+        copyright: "All rights reserved"
+    }
+}
---
+++ src/translations/main.fr.js
@@ -0,0 +1,44 @@
+export default {
+    nav: {
+        contact: "Nous contacter ",
+        vat: "Tous les prix affiché sont hors taxes",
+        menu: {
+            product_index: "Nos aéronefs",
+            ask: "Demander des informations"
+        }
+    },
+    hero: {
+        tagline: "Le futur de l'aviation privée est déjà là",
+        cta: "Accéder au futur"
+    },
+    product_index: {
+        title: "Nos avions",
+        tagline: "À la une",
+    },
+    product: {
+        price: "Dès {value} CHF",
+    },
+    ask: {
+        name: {
+            placeholder: "Votre nom",
+            label: "Nom complet"
+        },
+        phone: {
+            placeholder: "Votre numéro de téléphone",
+            label: "Numéro de téléphone"
+        },
+        content: {
+            placeholder: "Votre demande",
+            label: "Message"
+        },
+        button: "Envoyer",
+        policy: {
+            label: "Nous n'envoyons pas de spam aux clients. Vérifiez notre",
+            link: "fr",
+            link_label: "Politique de confidentialité"
+        }
+    },
+    footer: {
+        copyright: "Tous droits réservés"
+    }
+}
