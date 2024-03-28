f97ed578061dd757cb1cccc6697ff6065db8bf6b
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-03-28 11:24:06 +0100

    test: move common test logic to beforeEach order

--- src/test/localization/localization.test.js
+++ src/test/localization/localization.test.js
@@ -11,55 +11,68 @@ describe("When user change the language of the page", () => {
      */
     let driver;
 
+    const testedElement = 'h1.subtitle'
+
+    let selector;
+    let element;
+
     beforeEach(async () => {
         driver = await new Builder().forBrowser('chrome').build();
+
+        await driver.get(APP_URL);
+
         setTimeout(async () => {
             await haveLanguageSelector(driver);
         }, 5000)
+
+        selector = new Select(await driver.findElement(By.id('language-selector')));
+
+        element = await driver.findElement(By.css(testedElement))
     })
 
     it('should translate the page to english', async () => {
-        await driver.get(APP_URL);
-        await isNotTranslated(driver, 'The future of private aviation is already here')
+        // Given
+        await selector.selectByValue('fr')
+        const testedText = 'The future of private aviation is already here'
+
+        await isNotTranslated(driver, testedText)
 
         // When
-        let languageSelector = new Select(await driver.findElement(By.css('#language-selector')));
-        await languageSelector.selectByValue('en')
+        await selector.selectByValue('en')
 
         // Then
-        let recievedText = await driver.findElement(By.css('h1.subtitle')).getText();
-        expect(recievedText).toBe('The future of private aviation is already here')
+        let recievedText = await element.getText();
+        expect(recievedText).toBe(testedText)
     });
 
     it('should translate the page to french', async () => {
-        await driver.get(APP_URL);
-        await isNotTranslated(driver, 'Le futur de l\'aviation privée est déjà là')
+        // Given
+        const testedText = 'Le futur de l\'aviation privée est déjà là'
 
         // When
-        let languageSelector = new Select(await driver.findElement(By.id('language-selector')));
-        await languageSelector.selectByValue('fr')
+        await selector.selectByValue('fr')
 
         // Then
-        let recievedText = await driver.findElement(By.css('h1.subtitle')).getText();
-        expect(recievedText).toBe('Le futur de l\'aviation privée est déjà là')
+        let recievedText = await element.getText();
+        expect(recievedText).toBe(testedText)
+
     });
 
     it('should translate the page to german', async () => {
-        await driver.get(APP_URL);
-        await isNotTranslated(driver, 'Die Zukunft der Privatfliegerei ist bereits da')
+        // Given
+        const testedText = 'Die Zukunft der Privatfliegerei ist bereits da'
+
+        await isNotTranslated(driver, testedText)
 
         // When
-        let languageSelector = new Select(await driver.findElement(By.id('language-selector')));
-        await languageSelector.selectByValue('de')
+        await selector.selectByValue('de')
 
         // Then
-        let recievedText = await driver.findElement(By.css('h1.subtitle')).getText();
-        expect(recievedText).toBe('Die Zukunft der Privatfliegerei ist bereits da')
+        let recievedText = await element.getText();
+        expect(recievedText).toBe(testedText)
     });
 
     it("should'nt whipe input data while translating page", async () => {
-        await driver.get(APP_URL);
-
         let input = await driver.findElement(By.css('input[type="text"]'));
         expect(input).toBeDefined();
 
@@ -68,8 +81,7 @@ describe("When user change the language of the page", () => {
         await input.sendKeys(inputContent)
 
         // When
-        let languageSelector = new Select(await driver.findElement(By.id('language-selector')));
-        await languageSelector.selectByValue('it')
+        await selector.selectByValue('fr')
 
         // Then
         expect(await input.getAttribute("value")).toBe(inputContent)
