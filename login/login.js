3e65a9701a3e60f37c9873b73f0278619845851e
Benjamin Fontana <ben192003@gmail.com>
2024-03-28 12:14:48 +0100

    feat: add event listen on button click

--- login/login.js
+++ login/login.js
@@ -1,15 +1,27 @@
-import './login.scss'
+import "./login.scss";
+import $ from "jquery";
+import Authentication from "../src/models/Authentication.js";
+import FBConnector from "../src/models/FBConnector.js";
 
-$(function() {
-    'use strict';
+$(function () {
+	"use strict";
 
-    $('.form-control').on('input', function() {
-        var $field = $(this).closest('.form-group');
-        if (this.value) {
-            $field.addClass('field--not-empty');
-        } else {
-            $field.removeClass('field--not-empty');
-        }
-    });
+	$(".form-control").on("input", function () {
+		var $field = $(this).closest(".form-group");
+		if (this.value) {
+			$field.addClass("field--not-empty");
+		} else {
+			$field.removeClass("field--not-empty");
+		}
+	});
+});
+const auth = new Authentication(new FBConnector());
 
+$("#login-button").on("click", (event) => {
+	event.preventDefault();
+    auth.login();
+});
+$("#logout-button").on("click", (event) => {
+	event.preventDefault();
+	auth.logout();
 });
