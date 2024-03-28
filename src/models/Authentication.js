d5cc47e2dc62f439fcbf561ccb62fc065493001e
Benjamin Fontana <ben192003@gmail.com>
2024-03-28 11:00:23 +0100

    fix: now uses a constructor and use a private property

--- src/models/Authentication.js
+++ src/models/Authentication.js
@@ -1,14 +1,15 @@
 module.exports = class Authentication {
-	Authentication(connector) {
-        this.connector = connector;
+    #connector;
+	constructor(connector) {
+        this.#connector = connector;
     }
 	login() {
-        this.connector.login();
+        this.#connector.login();
     }
 	logout() {
-        this.connector.logout();
+        this.#connector.logout();
     }
 	isUserLoggedIn() {
-        return this.connector.isUserLoggedIn();
+        return this.#connector.checkUserLoginStatus();
     }
 };
